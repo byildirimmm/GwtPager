4a5ad069eb9c610aab2c3b704f71c135c7185871
byildirimmm <burakyildirim09@gmail.com>
2014-12-30 15:39:27 +0200

    create

---
+++ GwtPager/src/com/gwt/kyu/client/MyEntry.java
@@ -0,0 +1,13 @@
+package com.gwt.kyu.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class MyEntry implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ GwtPager/src/com/gwt/kyu/client/presenters/Presenter.java
@@ -0,0 +1,9 @@
+package com.gwt.kyu.client.presenters;
+
+import com.google.gwt.user.client.ui.HasWidgets;
+
+public interface Presenter {
+	public void bind();
+	public void go(HasWidgets container);
+
+}
