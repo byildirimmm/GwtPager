31dfe68bc56c4be525001a7817e126c9b38ec50b
byildirimmm <burakyildirim09@gmail.com>
2014-12-30 15:56:19 +0200

    alooooooo

---
+++ GwtPager/src/com/gwt/kyu/shared/Branch.java
@@ -0,0 +1,28 @@
+package com.gwt.kyu.shared;
+
+public class Branch {
+	private String branchID;
+	private String branchName;
+	private String branchLocation;
+	
+	
+	
+	public String getBranchID() {
+		return branchID;
+	}
+	public void setBranchID(String branchID) {
+		this.branchID = branchID;
+	}
+	public String getBranchName() {
+		return branchName;
+	}
+	public void setBranchName(String branchName) {
+		this.branchName = branchName;
+	}
+	public String getBranchLocation() {
+		return branchLocation;
+	}
+	public void setBranchLocation(String branchLocation) {
+		this.branchLocation = branchLocation;
+	}
+}
---
+++ GwtPager/src/com/gwt/kyu/shared/Customer.java
@@ -0,0 +1,44 @@
+package com.gwt.kyu.shared;
+
+public class Customer {
+	
+	private String customerName;
+	private String customerID;
+	private String customerSurname;
+	private String customerMail;
+	private String customerBranch;
+	
+	
+	
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getCustomerID() {
+		return customerID;
+	}
+	public void setCustomerID(String customerID) {
+		this.customerID = customerID;
+	}
+	public String getCustomerSurname() {
+		return customerSurname;
+	}
+	public void setCustomerSurname(String customerSurname) {
+		this.customerSurname = customerSurname;
+	}
+	public String getCustomerMail() {
+		return customerMail;
+	}
+	public void setCustomerMail(String customerMail) {
+		this.customerMail = customerMail;
+	}
+	public String getCustomerBranch() {
+		return customerBranch;
+	}
+	public void setCustomerBranch(String customerBranch) {
+		this.customerBranch = customerBranch;
+	}
+
+}
